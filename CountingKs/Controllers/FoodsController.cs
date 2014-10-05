010f3e331244260681bd73d8b6955067ecf4b507
ktundwal@gmail.com <denver>
2014-10-04 23:26:00 -0700

    Food model

--- CountingKs/Controllers/FoodsController.cs
+++ CountingKs/Controllers/FoodsController.cs
@@ -1,5 +1,6 @@
 ﻿using CountingKs.Data;
 using CountingKs.Data.Entities;
+using CountingKs.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,21 +12,29 @@ namespace CountingKs.Controllers
 {
     public class FoodsController : ApiController
     {
-        public IEnumerable<Food> Get()
-        {
-            var repo = new CountingKsRepository(new CountingKsContext());
+        ICountingKsRepository _repo;
+        ModelFactory _modelFactory;
 
-            var results = repo.GetAllFoods()
+        public FoodsController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+            _modelFactory = new ModelFactory();
+        }
+
+        public IEnumerable<FoodModel> Get()
+        {
+            var results = _repo.GetAllFoodsWithMeasures()
                               .OrderBy(f => f.Description)
                               .Take(25)
-                              .ToList();
+                              .ToList()
+                              .Select(f => _modelFactory.Create(f));
 
             return results;
         }
 
-        public object Get(int id)
+        public FoodModel Get(int id)
         {
-            return new { name = "value", id = id };
+            return _modelFactory.Create(_repo.GetFood(id));
         }
     }
 }
---
+++ CountingKs/Models/ModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using CountingKs.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountingKs.Models
+{
+    public class ModelFactory
+    {
+        public FoodModel Create(Food food)
+        {
+            return new FoodModel()
+            {
+                Description = food.Description,
+                Measures = food.Measures.Select(m => Create(m))
+            };
+        }
+
+        public MeasureModel Create(Measure measure)
+        {
+            return new MeasureModel()
+            {
+                Description = measure.Description,
+                Calories = Math.Round(measure.Calories)
+            };
+        }
+    }
+}
